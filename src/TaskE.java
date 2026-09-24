674af37fd223e7790af6af9579f43879dafdb857
Student <student@gmail.com>
2026-09-24 11:52:19 +0600

    hytjcuttjrtj

---
+++ src/TaskE.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TaskE {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int v = scanner.nextInt();
+        int t = scanner.nextInt();
+
+        int distance = v * t;
+
+        int position = (distance % 109 + 109) % 109;
+
+        System.out.println(position);
+
+        scanner.close();
+    }
+}
